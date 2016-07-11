a1e1f71ef5b3cb9c964e4ada89c2aa791a40ce7c
Brad Leege <bleege@gmail.com>
2016-07-10 23:22:50 -0700

    Initial setup of MainActivityTest

---
+++ app/src/androidTest/java/com/bradleege/geomocktest/view/MainActivityTest.java
@@ -0,0 +1,27 @@
+package com.bradleege.geomocktest.view;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import com.bradleege.geomocktest.R;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+
+    @Rule
+    public final ActivityTestRule<MainActivity> main = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testSanity() {
+        main.launchActivity(null);
+        onView(withId(R.id.geocodeResultTextView)).check(matches(withText("Georesults Here!")));
+    }
+}
