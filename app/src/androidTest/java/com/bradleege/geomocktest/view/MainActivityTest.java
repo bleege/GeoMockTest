a9b3043ee00ac522a1c32c0b3c68d33cc8599a01
Brad Leege <bleege@gmail.com>
2016-07-11 12:00:55 -0700

    Adding logging and removing exported flag

--- app/src/androidTest/java/com/bradleege/geomocktest/view/MainActivityTest.java
+++ app/src/androidTest/java/com/bradleege/geomocktest/view/MainActivityTest.java
@@ -6,6 +6,7 @@ import com.bradleege.geomocktest.R;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import timber.log.Timber;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -17,8 +18,16 @@ public class MainActivityTest {
     @Rule
     public final ActivityTestRule<MainActivity> main = new ActivityTestRule<>(MainActivity.class);
 
+    public MainActivityTest() {
+        super();
+        Timber.tag("MainActivityTest");
+    }
+
     @Test
     public void testSanity() {
+        Timber.i("testSanity() started...");
         onView(withId(R.id.geocodeResultTextView)).check(matches(withText("Georesults Here!")));
+        Timber.i("testSanity() finished.");
     }
+
 }
